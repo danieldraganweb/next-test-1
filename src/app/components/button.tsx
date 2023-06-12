a52194bf8fc6ab01ef8a5158ce075782b78e2cb7
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-06-12 11:49:35 +0200

    button component created and imported to homepage

---
+++ src/app/components/button.tsx
@@ -0,0 +1,11 @@
+function Button() {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      I feel lucky!
+    </button>
+  );
+}
+
+export default Button;
+
+// Path: src\app\components\button.tsx
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Button from "./components/button";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-6xl font-bold text-center">
         Welcome to <a href="https://nextjs.org">Next.js!</a>
       </h1>
+      <Button />
     </main>
   );
 }
